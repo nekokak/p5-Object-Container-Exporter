477593e04d48bd6ffccaffdab2302aebe8d93990
Hiroyuki Akabane <hirobanex@gmail.com>
2011-11-25 01:08:02 +0900

    add pod

--- lib/Object/Container/Exporter.pm
+++ lib/Object/Container/Exporter.pm
@@ -158,21 +158,232 @@ __END__
 
 =head1 NAME
 
-Object::Container::Exporter -
+Object::Container::Exporter - strong showcut to your classes.
 
 =head1 SYNOPSIS
 
-  use Object::Container::Exporter;
+    #your application tree
+    └── MyApp
+        ├── Api
+        │   ├── Form
+        │   │   └── Foo.pm
+        │   └── User.pm
+        ├── Container.pm
+        └── Foo.pm
+
+    #your sub class
+    package MyApp::Container;
+    use Object::Container::Exporter -base;
+
+    register_namespace form => 'Mock::Api::Form';
+
+    register 'foo' => sub {
+        my $self = shift;
+        $self->load_class('Mock::Foo');
+        Mock::Foo->new;
+    };
+
+    #your main script
+    use MyApp::Container qw/api form/;
+    
+    container('foo')->say;
+
+    my $row = api('User')->fetch;
+    
+    form('foo')->fillin($row->get_columns);
 
 =head1 DESCRIPTION
 
-Object::Container::Exporter is
+Object::Container::Exporter is object container like L<Object::Container>.
+The difference is that it has bulk registering the class object in your indeicate directory with container.
+
+=head1 Bulk registering your indicated directory's class objects
+
+Object::Container::Exporter provide the shortcut function to call the class object in your application' second directory.There is no 'use' and 'new' in your main script to access your class object.In this case, you must indicate the decamelized directory name as export function.
+
+Examples are:
+
+    #your application tree
+
+    └── MyApp
+        ├── Api
+        │   └── Password.pm
+        └── Container.pm
+
+    #your sub class
+    package MyApp::Container;
+    use Object::Container::Exporter -base;
+
+    #your main script
+    use MyApp::Container qw/api/;
+    
+    my $hash_val = api('Password')->generate($pass);
+
+When you wanna export shortcut function to call the class object in any directory, you can register your original shortcut function in the sub class.
+
+Examples are:
+
+    #your application tree
+    └── MyApp
+        ├── Model
+        │   ├── Api
+        │   │   └── User.pm
+        │   └── Command
+        │   　   └── Password.pm
+        └── Container.pm
+
+    #your sub class
+    package MyApp::Container;
+    use Object::Container::Exporter -base;
+
+    register_namespace api => 'Mock::Model::Api::User';
+    register_namespace cmd => 'Mock::Model::Api::Password';
+
+    #your main script
+    use MyApp::Container qw/api cmd/;
+    
+    my $hash_val = cmd('Password')->generate($pass);
+
+    my $row = api('User')->register(
+        id   => 'nekokak',
+        pass => $hash_val,
+    );
+
+Now, you have efficiently fun life development.
+
+=head1 METHODS
+
+=head2 register
+
+Register classes to container.
+
+Examples are:
+
+    package MyApp::Container;
+    use Object::Container::Exporter -base;
+
+    #register($register_class_name,$code);
+    register db => sub {
+        my $self = shift;
+        $self->load_class('DBI');
+        DBI->connect('dbi:mysql:sandbox', 'root', 'pass', +{
+            mysql_enable_utf8 => 1,
+            PrintError        => 0,
+            RaiseError        => 1,
+        },);
+    };
+
+    #register($load_class,@opts);
+    register 'DBI' => ('dbi:mysql:sandbox', 'root', 'pass', +{
+        mysql_enable_utf8 => 1,
+        PrintError        => 0,
+        RaiseError        => 1,
+    },);
+
+=head2 register_namespace
+
+You can register your original function name to call your application calss objects.
+
+Example is:
+
+    package MyApp::Container;
+    use Object::Container::Exporter -base;
+
+    register_namespace form => 'MyApp::Api::Form';
+
+=head2 register_default_container_name
+
+To call the object registered your sub class, the 'container' function exported. But you can change the export function name.
+
+Example is:
+
+    #your sub class
+    package MyApp::Container;
+    use Object::Container::Exporter -base;
+
+    register_default_container_name 'con';
+
+    register db => sub {
+        my $self = shift;
+        $self->load_class('DBI');
+        DBI->connect('dbi:mysql:sandbox', 'root', 'pass',);
+    };
+
+    #your main script
+    use MyApp::Container;
+    
+    my $user_bodys = con('db')->selectcolcall_arrayref('SELECT body FROM user');
+
+=head2 get
+
+Get the object that registered by 'register' method.
+
+Examples are:
+    #your sub class
+    package MyApp::Container;
+    use Object::Container::Exporter -base;
+
+    register dbh => sub {
+        my $self = shift;
+        $self->load_class('DBI');
+        DBI->connect('dbi:mysql:sandbox', 'root', 'pass',);
+    };
+
+    register teng => sub {
+        my $self = shift;
+        $self->load_class('MyApp::DB');
+
+        MyApp::DB->new(
+            dbh => $self->get('dbh'),#get registered object
+        );
+    };
+
+    #your main script
+    use MyApp::Container -no_export;
+    
+    my $obj = MyApp::Container->instance;
+
+    my $row = $obj->get('teng')->single('user');
+
+    ##or
+    use MyApp::Container;
+
+    my $obj = container;
+
+    my $row = $obj->get('teng')->single('user');
+
+
+=head2 remove
+
+Remove the cached object that is created at C<get> method above.
+Return value is the deleted object if it's exists.
+
+=head2 load_class
+
+Like require function.
+
+Example is:
+
+    package MyApp::Container;
+    use Object::Container::Exporter -base;
+
+    register db => sub {
+        my $self = shift;
+        $self->load_class('DBI');#load when call this code reference.
+        DBI->connect('dbi:mysql:sandbox', 'root', 'pass',);
+    };
 
 =head1 AUTHOR
 
 Atsushi Kobayashi E<lt>nekokak _at_ gmail _dot_ comE<gt>
 
+=head1 THANKS
+
+Much of this documentation was taken from Object::Container
+
 =head1 SEE ALSO
+
+L<Object::Container>.
 
 =head1 LICENSE
 
@@ -180,3 +391,4 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
